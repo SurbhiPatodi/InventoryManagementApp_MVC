d16379fb728edbc8747026260d8961bdaaeae93f
Surbhi Patodi <surbhi.patodi@iqvia.com>
2025-01-16 18:10:22 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InventoryManagementApp_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
